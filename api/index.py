dc1e2faa1a9b53d1e8d334ed60581e2532141294
santahora <155487474+santahora@users.noreply.github.com>
2025-07-15 20:16:06 -0300

    .

--- api/index.py
+++ api/index.py
@@ -9,7 +9,7 @@ def read_root():
 
 
 
-dados_missas = {
+dados_missas_dic = {
     1: {'Paróquia': 'CATEDRAL - PARÓQUIA SAGRADO CORAÇÃO DE JESUS', 'Arquidiocese': 'Londrina', 'Decanato': 'Centro', 'Cidade': 'Londrina', 'Dia': 'Segunda', 'Horário': '07h00', 'Observação': nan},
     2: {'Paróquia': 'CATEDRAL - PARÓQUIA SAGRADO CORAÇÃO DE JESUS', 'Arquidiocese': 'Londrina', 'Decanato': 'Centro', 'Cidade': 'Londrina', 'Dia': 'Segunda', 'Horário': '12h00', 'Observação': nan},
     3: {'Paróquia': 'CATEDRAL - PARÓQUIA SAGRADO CORAÇÃO DE JESUS', 'Arquidiocese': 'Londrina', 'Decanato': 'Centro', 'Cidade': 'Londrina', 'Dia': 'Terça', 'Horário': '07h00', 'Observação': nan},
@@ -21,7 +21,7 @@ dados_missas = {
 # Rota dinâmica (GET)
 @app.get("/paroquias")
 def lista_paroquias():
-    return dados_missas
+    return dados_missas_dic
 
 
 

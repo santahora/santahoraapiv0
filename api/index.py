4ddf3836be57601baadc8f3e02d92f7789f144c4
santahora <155487474+santahora@users.noreply.github.com>
2025-07-15 18:28:48 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,12 +1,66 @@
+# from fastapi import FastAPI
+# from fastapi.responses import JSONResponse
+# from mangum import Mangum  # adaptador para serverless
+
+# app = FastAPI()
+
+# @app.get("/")
+# def read_root():
+#     return JSONResponse(content={"message": "API está online!"})
+
+# # adaptador para Vercel
+# # handler = Mangum(app)
+
+
+
+
+
+####
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from mangum import Mangum  # adaptador para serverless
+from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+import numpy as np
+from mangum import Mangum
+
 
 app = FastAPI()
 
+
+# Configurar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todas as origens
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todos os cabeçalhos
+)
+
+
+def load_data(url):
+    df = pd.read_csv(url)
+    return df
+
+df = load_data('https://raw.githubusercontent.com/santahora/santahora/main/horarios_missas_id_2.csv')
+
+# Rota de teste (GET)
 @app.get("/")
 def read_root():
-    return JSONResponse(content={"message": "API está online!"})
+    return {"mensagem": "API is up!"}
 
-# adaptador para Vercel
+# Rota dinâmica (GET)
+@app.get("/missas/paroquia/{nome_paroquia}")
+def missas_paroquia(nome_paroquia: str):
+    df_cut = df[df['Paróquia'] == nome_paroquia]
+    df_cut = df_cut.replace({np.nan: None})
+
+    return df_cut.set_index('ID missa').to_dict(orient='index')
+
+
+# Rota dinâmica (GET)
+@app.get("/paroquias")
+def lista_paroquias():
+    dic_lista_paroquias = {'lista_paroquias': list(df['Paróquia'].unique())}
+    return dic_lista_paroquias
+
+# Adaptador Mangum para Vercel Serverless Functions
 # handler = Mangum(app)

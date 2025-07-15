d29500e7ed5c44cc86739bb57f7cc5cc9200541b
santahora <155487474+santahora@users.noreply.github.com>
2025-07-15 20:14:57 -0300

    .

--- api/index.py
+++ api/index.py
@@ -9,20 +9,19 @@ def read_root():
 
 
 
-dados_missas = pd.DataFrame({
+dados_missas = {
     1: {'Paróquia': 'CATEDRAL - PARÓQUIA SAGRADO CORAÇÃO DE JESUS', 'Arquidiocese': 'Londrina', 'Decanato': 'Centro', 'Cidade': 'Londrina', 'Dia': 'Segunda', 'Horário': '07h00', 'Observação': nan},
     2: {'Paróquia': 'CATEDRAL - PARÓQUIA SAGRADO CORAÇÃO DE JESUS', 'Arquidiocese': 'Londrina', 'Decanato': 'Centro', 'Cidade': 'Londrina', 'Dia': 'Segunda', 'Horário': '12h00', 'Observação': nan},
     3: {'Paróquia': 'CATEDRAL - PARÓQUIA SAGRADO CORAÇÃO DE JESUS', 'Arquidiocese': 'Londrina', 'Decanato': 'Centro', 'Cidade': 'Londrina', 'Dia': 'Terça', 'Horário': '07h00', 'Observação': nan},
     4: {'Paróquia': 'CATEDRAL - PARÓQUIA SAGRADO CORAÇÃO DE JESUS', 'Arquidiocese': 'Londrina', 'Decanato': 'Centro', 'Cidade': 'Londrina', 'Dia': 'Terça', 'Horário': '12h00', 'Observação': nan},
     5: {'Paróquia': 'CATEDRAL - PARÓQUIA SAGRADO CORAÇÃO DE JESUS', 'Arquidiocese': 'Londrina', 'Decanato': 'Centro', 'Cidade': 'Londrina', 'Dia': 'Quarta', 'Horário': '07h00', 'Observação': nan},
-})
+}
 
 
 # Rota dinâmica (GET)
 @app.get("/paroquias")
 def lista_paroquias():
-    dic_lista_paroquias = {'lista_paroquias': list(dados_missas['Paróquia'].unique())}
-    return dic_lista_paroquias
+    return dados_missas
 
 
 
